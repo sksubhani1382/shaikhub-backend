9431c28df75cb64257812778507cd896d4acd02a
sksubhani1382 <sksubhani1382@gmail.com>
2025-11-14 13:25:39 +0530

    Fixed CORS for GitHub Pages

--- src/main/java/com/shaikhub/config/WebConfig.java
+++ src/main/java/com/shaikhub/config/WebConfig.java
@@ -11,16 +11,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${frontend.url}")
-    private String frontendUrl;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl, 
-                                "http://127.0.0.1:5500", 
-                                "http://localhost:5500")
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedOrigins(
+                        "https://sksubhani1382.github.io",
+                        "https://sksubhani1382.github.io/",
+                        "http://localhost:5500",
+                        "http://127.0.0.1:5500"
+                )
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
