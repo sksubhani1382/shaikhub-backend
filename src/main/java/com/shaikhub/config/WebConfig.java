9846c8f36f4f1448b69b22db7f08a9376f527139
sksubhani1382 <sksubhani1382@gmail.com>
2026-02-06 17:41:49 +0530

    Fix database config

--- src/main/java/com/shaikhub/config/WebConfig.java
+++ src/main/java/com/shaikhub/config/WebConfig.java
@@ -19,8 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "https://sksubhani1382.github.io",
                         "https://sksubhani1382.github.io/",
                         "http://localhost:5500",
-                        "http://127.0.0.1:5500"
-                )
+                        "http://127.0.0.1:5500")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
